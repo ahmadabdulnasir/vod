ca24df74ff2c4929d266a18a1342926aa24998b4
Ahmad Abdulnasir Shuaib <ahmadabdulnasir9@gmail.com>
2021-11-20 08:29:06 +0100

    fix typo

--- vod/api/views.py
+++ vod/api/views.py
@@ -358,7 +358,7 @@ class MovieCreateAPIView(APIView):
 
         try:
             # serializer = self.get_serializer(data=request.data)
-            serializer = self.get_serializer(data=data)
+            serializer = self.serializer_class(data=data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             # 
